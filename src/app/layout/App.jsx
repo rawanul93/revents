99bebf4e3e61ff517b9df8743a7837b50271726c
Rawanul Momen <rusaf93@gmail.com>
2019-12-11 02:43:39 +0600

    End of Section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,18 +1,20 @@
-import React, {Component} from 'react';
-import { Button } from 'semantic-ui-react';
+import React, {Component, Fragment} from 'react';
+import EventDashboard from '../../features/event/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/navBar/NavBar';
+import { Container } from 'semantic-ui-react';
 
+//Fragment doesnt show up in our retunred HTML
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Re-vents</h1>
-        <button className="ui icon button">
-          <i className="smile icon"></i> 
-          CSS Button
-        </button>
-       <Button icon="smile" content="React button"/>
-      </div>
+      <Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+    
+      </Fragment>
     );
   }
    
