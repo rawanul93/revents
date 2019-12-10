7b71be3806af15714ce5fdac8b7136964ec553cb
Rawanul Momen <rusaf93@gmail.com>
2019-12-10 23:34:05 +0600

    Third commit

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Button } from 'semantic-ui-react';
 
 
 class App extends Component {
@@ -6,6 +7,11 @@ class App extends Component {
     return (
       <div>
         <h1>Re-vents</h1>
+        <button className="ui icon button">
+          <i className="smile icon"></i> 
+          CSS Button
+        </button>
+       <Button icon="smile" content="React button"/>
       </div>
     );
   }
