224952c9dab21e23a4be32bea98519fd75979ea6
Rawanul Momen <rusaf93@gmail.com>
2020-01-24 02:17:03 +0600

    End of Section 16 (1)

--- src/features/user/settings/AboutPage.jsx
+++ src/features/user/settings/AboutPage.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Button, Divider, Form, Header, Segment } from 'semantic-ui-react';
-import { Field, reduxForm } from 'redux-form';
-import RadioInput from '../../../app/common/form/RadioInput';
-import TextInput from '../../../app/common/form/TextInput';
-import TextArea from '../../../app/common/form/TextArea';
-import PlaceInput from '../../../app/common/form/PlaceInput';
-import SelectInput from '../../../app/common/form/SelectInput';
+import React from "react";
+import { Button, Divider, Form, Header, Segment } from "semantic-ui-react";
+import { Field, reduxForm } from "redux-form";
+import RadioInput from "../../../app/common/form/RadioInput";
+import TextInput from "../../../app/common/form/TextInput";
+import TextArea from "../../../app/common/form/TextArea";
+import PlaceInput from "../../../app/common/form/PlaceInput";
+import SelectInput from "../../../app/common/form/SelectInput";
 
 const interests = [
-  { key: 'drinks', text: 'Drinks', value: 'drinks' },
-  { key: 'culture', text: 'Culture', value: 'culture' },
-  { key: 'film', text: 'Film', value: 'film' },
-  { key: 'food', text: 'Food', value: 'food' },
-  { key: 'music', text: 'Music', value: 'music' },
-  { key: 'travel', text: 'Travel', value: 'travel' }
+  { key: "drinks", text: "Drinks", value: "drinks" },
+  { key: "culture", text: "Culture", value: "culture" },
+  { key: "film", text: "Film", value: "film" },
+  { key: "food", text: "Food", value: "food" },
+  { key: "music", text: "Music", value: "music" },
+  { key: "travel", text: "Travel", value: "travel" }
 ];
 
 const AboutPage = ({ pristine, submitting, handleSubmit, updateProfile }) => {
@@ -24,7 +24,13 @@ const AboutPage = ({ pristine, submitting, handleSubmit, updateProfile }) => {
       <Form onSubmit={handleSubmit(updateProfile)}>
         <Form.Group inline>
           <label>Tell us your status: </label>
-          <Field name="status" component={RadioInput} type="radio" value="single" label="Single" />
+          <Field
+            name="status"
+            component={RadioInput}
+            type="radio"
+            value="single"
+            label="Single"
+          />
           <Field
             name="status"
             component={RadioInput}
@@ -61,15 +67,24 @@ const AboutPage = ({ pristine, submitting, handleSubmit, updateProfile }) => {
         <Field
           width={8}
           name="origin"
-          options={{ types: ['(regions)'] }}
+          options={{ types: ["(regions)"] }}
           component={PlaceInput}
           placeholder="Country of Origin"
         />
         <Divider />
-        <Button disabled={pristine || submitting} size="large" positive content="Update Profile" />
+        <Button
+          disabled={pristine || submitting}
+          size="large"
+          positive
+          content="Update Profile"
+        />
       </Form>
     </Segment>
   );
 };
 
-export default reduxForm({ form: 'userProfile', enableReinitialize: true, destroyOnUnmount: false })(AboutPage);
+export default reduxForm({
+  form: 'userProfile',
+  enableReinitialize: true,
+  destroyOnUnmount: false
+})(AboutPage);
