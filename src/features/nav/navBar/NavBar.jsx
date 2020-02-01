dd965f29d92e4968d58aa27647ae202d8d2ff349
Rawanul Momen <rusaf93@gmail.com>
2020-02-01 20:07:56 +0600

    Start Challenge1

--- src/features/nav/navBar/NavBar.jsx
+++ src/features/nav/navBar/NavBar.jsx
@@ -63,6 +63,7 @@ class NavBar extends Component {
             <SignedInMenu
               signOut={this.handleSignOut}
               profile={profile}
+              auth={auth}
             />
           ) : (
             <SignedOutMenu
